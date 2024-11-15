a2cb814b6b245653e52da7f977785e07d86be68c
robuzzmusk <167455571+robuzzmusk@users.noreply.github.com>
2024-11-15 09:17:34 +0530

    Update shell_script.sh

--- shell_script.sh
+++ shell_script.sh
@@ -114,3 +114,29 @@ if [ $? -eq 0 ]; then
 else
     echo "incorrect command"
 fi
+####################################
+#Using “$*” Enclosing the variable $* with double quotes means all arguments will be treated as a single string
+#Using “$@” The variable $@ treats each command-line argument as a separate element in an array
+#!/bin/bash
+
+# Check if any arguments are passed
+if [ $# -gt 0 ]; then
+    # Check if AWS CLI is installed and working
+    aws --version
+    if [ $? -eq 0 ]; then
+        # Loop through each region passed as argument
+        for REGION in "$@"; do
+            echo "Describing VPCs in region: $REGION"
+            aws ec2 describe-vpcs --region ${REGION} | jq ".Vpcs[].VpcId" -r
+            echo "____________________________________________________________________________________________"
+        done
+    else
+        echo "AWS CLI is not installed or not working."
+    fi
+else
+    echo "No regions provided. Please provide one or more AWS regions."
+fi
+
+# Print number of arguments passed
+echo "Number of arguments: $#"
+####################################
